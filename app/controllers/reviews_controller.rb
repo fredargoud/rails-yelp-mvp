72e76f0a76a8a1971883b8491ef58d04edf24b7a
Fred Argoud <fred.argoud@gmail.com>
2018-11-14 17:30:07 +0100

    Restaurant

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create, :index]
+
+  def index
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    @review.restaurant = @restaurant
+    @review.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
